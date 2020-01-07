4d6e87864810b0ac23ecc45d2fac6dffa4646d6a
Brandon Weaver <a7xncob@gmail.com>
2020-01-07 04:31:29 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,5 @@
 class User < ActiveRecord::Base
   
   has_secure_password
-  attr_accessor :balance
-  @balance = 0
-  
-  def deposit(amount)
-    @balance += amount
-  end
-  
-  def withdrawal(amount)
-    if amount <= @balance
-      @balance -= amount
-    end
-  end
   
 end
